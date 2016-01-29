08d4e6d71d0950c4656ce7dde5a29b5b63f51faa
RichardGosse <rwgosse@gmail.com>
2016-01-29 15:20:11 -0800

    fixed link 6 (dunno) with picture of beaker Signed-off-by:RichardGosse <rwgosse@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -45,5 +45,14 @@ $route['lock/(:any)/(:any)'] = "welcome/shucks";
 $route['sleep'] = "first/zzz";
 $route['show/(:num)'] = "first/gimme/$1";
 
+$route['dunno'] = function() {
+	$source = './data/beaker.jpg'; // an image you provide
+	// set the mime type for that image
+	header("Content-type: image/jpeg"); 
+	header('Content-Disposition: inline');
+	readfile($source); // dish it
+	die(); // and we don't have to go any further
+};
+
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
